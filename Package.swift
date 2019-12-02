00f50bc59dacd55f26ebe86ad48705ccaa081026
Murat Mayadağ <murat-2@hotmail.com>
2019-12-02 11:12:08 +0300

    :tada: auth server created

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "auth",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
+        .package(url: "https://github.com/IBM-Swift/Kitura.git",from:"2.9.1"),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from:"1.9.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git" ,from:"2.5.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git",from:"2.1.3"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git",from:"3.2.0")
+    ],
+    targets:[
+        .target(
+            name:"auth",
+            dependencies:["Kitura","HeliumLogger","Credentials","CredentialsHTTP","PerfectCrypto"]
+        )
     ]
 )
